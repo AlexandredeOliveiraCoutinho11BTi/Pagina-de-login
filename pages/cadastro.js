fa23f4b5f46318d0b83fa2bdc0a6c5c5ab54c140
AlexandredeOliveiraCoutinho11BTi <162030541+AlexandredeOliveiraCoutinho11BTi@users.noreply.github.com>
2025-12-02 08:14:04 -0300

    Update cadastro.js

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -1,30 +1,31 @@
-const form = document.querySelector("#cadastroForm");
+const button = document.querySelector("button")
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    sendUser();
-});
+button.onclick = (event) => {
+    event.preventDefault()
+    sendUser()
+}
 
 async function sendUser() {
-    const name = document.querySelector("#nome").value;
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#senha").value;
+    const name = document.querySelector("#nome").value
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#senha").value
 
-    const user = { name, email, password };
-
-    try {
-        const response = await fetch("https://backend02mb-phi.vercel.app/cadastrar", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(user)
-        });
-
-        const data = await response.json();
-        alert(data.message);
-
-        window.location.href = "../index.html";
-    } catch (error) {
-        alert("Erro ao cadastrar.");
-        console.error(error);
+    const user = {
+        name,
+        email,
+        password
     }
+
+    const response = await fetch("https://backend02mb-phi.vercel.app/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ user })
+    }).then(response => response.json())
+
+    alert(response.message)
+
+    window.location.href = "../index.html"
+
 }
